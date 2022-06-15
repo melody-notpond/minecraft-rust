a483476a9c48e0f94e3eaab1610277c58148e640
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-06-15 19:44:15 -0400

    Copied a lot of stuff

--- src/client/bin/main.rs
+++ src/client/bin/main.rs
@@ -1,7 +1,7 @@
-use std::time::Instant;
+use std::time::{Instant, Duration};
 
-use glium::{glutin::{event::{Event, WindowEvent}, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder, ContextBuilder}, Display, Surface, VertexBuffer, Program, uniform, IndexBuffer, index::PrimitiveType, DrawParameters, Depth, DepthTest, BackfaceCullingMode};
-use minecraft_rust::client::rendering::{Vertex, self};
+use glium::{glutin::{event::{Event, WindowEvent, StartCause, VirtualKeyCode, ElementState}, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder, ContextBuilder, dpi::PhysicalPosition}, Display, Surface, VertexBuffer, Program, uniform, IndexBuffer, index::PrimitiveType, DrawParameters, Depth, DepthTest, BackfaceCullingMode, PolygonMode};
+use minecraft_rust::client::{rendering, camera::Camera};
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -18,34 +18,133 @@ fn main() {
     let fs_source = std::fs::read_to_string("src/client/shaders/fragment.glsl").unwrap();
     let program = Program::from_source(&display, &vs_source, &fs_source, None).unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        let next_frame_time = Instant::now() + std::time::Duration::from_nanos(16_666_667);
-        *control_flow = ControlFlow::WaitUntil(next_frame_time);
+    let mut params = DrawParameters {
+        depth: Depth {
+            test: DepthTest::IfLess,
+            write: true,
+            ..Default::default()
+        },
+        backface_culling: BackfaceCullingMode::CullClockwise,
+        ..Default::default()
+    };
 
-        if let Event::WindowEvent { event, .. } = event {
-            if let WindowEvent::CloseRequested = event {
-                *control_flow = ControlFlow::Exit;
-            }
-            return;
+    let mut camera = Camera::new(2.0, 0.01, 110.0);
+    let mut locked;
+
+    {
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
+        locked = window.set_cursor_grab(true).is_ok();
+
+        if locked {
+            let size = window.inner_size();
+            let centre = PhysicalPosition::new(size.width / 2, size.height / 2);
+            window.set_cursor_position(centre).unwrap();
+            window.set_cursor_visible(false);
         }
+    }
+
+    let mut frame_count = 0;
+    let mut last = Instant::now();
+    let mut last_frame = Instant::now();
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent { event, .. } => {
+                match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+
+                    WindowEvent::Focused(false) => {
+                        let gl_window = display.gl_window();
+                        let window = gl_window.window();
+                        window.set_cursor_visible(true);
+                        window.set_cursor_grab(false).unwrap();
+                        locked = false;
+                    }
+
+                    WindowEvent::KeyboardInput { input, .. }
+                        if matches!(input.virtual_keycode, Some(VirtualKeyCode::Escape))
+                            && matches!(input.state, ElementState::Released) =>
+                    {
+                        let gl_window = display.gl_window();
+                        let window = gl_window.window();
+                        let size = window.inner_size();
+                        let centre = PhysicalPosition::new(size.width / 2, size.height / 2);
+                        window.set_cursor_position(centre).unwrap();
+                        window.set_cursor_visible(locked);
+                        locked ^= true;
+                        window.set_cursor_grab(locked).unwrap();
+                    }
+
+                    WindowEvent::KeyboardInput { input, .. }
+                        if locked && camera.move_self(input) => {},
+
+                    WindowEvent::KeyboardInput { input, .. } if locked => {
+                        if let Some(VirtualKeyCode::Semicolon) = input.virtual_keycode {
+                            if input.state == ElementState::Released {
+                                match params.polygon_mode {
+                                    PolygonMode::Point => params.polygon_mode = PolygonMode::Line,
+                                    PolygonMode::Line => params.polygon_mode = PolygonMode::Fill,
+                                    PolygonMode::Fill => params.polygon_mode = PolygonMode::Point,
+                                }
+                            }
+                        }
+                    }
+
+                    WindowEvent::CursorMoved { position, .. } if locked => {
+                        let gl_window = display.gl_window();
+                        let window = gl_window.window();
+                        let size = window.inner_size();
+                        let centre = PhysicalPosition::new(size.width / 2, size.height / 2);
+                        camera.turn_self(
+                            position.x as i32 - centre.x as i32,
+                            position.y as i32 - centre.y as i32,
+                        );
+                        window.set_cursor_position(centre).unwrap();
+                    }
+                    _ => (),
+                }
+                return;
+            }
+
+            Event::NewEvents(cause) => match cause {
+                StartCause::Init => {
+                    last = Instant::now();
+                    last_frame = last;
+                    let next_frame_time = last + Duration::from_nanos(5);
+                    *control_flow = ControlFlow::WaitUntil(next_frame_time);
+                    return;
+                }
+
+                StartCause::ResumeTimeReached { .. } => (),
+
+                _ => return,
+            },
+
+            _ => return,
+        }
+
+        frame_count += 1;
+        if last - last_frame >= Duration::from_secs(1) {
+            println!("{} frames per second", frame_count);
+            frame_count = 0;
+            last_frame = last;
+        }
+
+        let delta = Instant::now() - last;
+        last = Instant::now();
+
+        camera.tick(delta);
 
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 1.0, 0.0, 1.0), 1.0);
 
-        let params = DrawParameters {
-            depth: Depth {
-                test: DepthTest::IfLess,
-                write: true,
-                ..Default::default()
-            },
-            backface_culling: BackfaceCullingMode::CullClockwise,
-            ..Default::default()
-        };
-
         let light = [-1.0, 0.4, 0.9f32];
         let uniforms = uniform! {
-            perspective: rendering::perspective_matrix(&target),
-            view: rendering::view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]),
+            perspective: camera.perspective(&target),
+            view: camera.view_matrix(),
             model: [
                 [0.01, 0.0, 0.0, 0.0],
                 [0.0, 0.01, 0.0, 0.0],
--- src/client/rendering.rs
+++ src/client/rendering.rs
@@ -1,63 +1,9 @@
-use glium::{Frame, Surface};
-
 #[derive(Copy, Clone)]
 pub struct Vertex {
     position: (f32, f32, f32)
 }
 
 implement_vertex!(Vertex, position);
-
-pub fn perspective_matrix(target: &Frame) -> [[f32; 4]; 4] {
-    let (width, height) = target.get_dimensions();
-    let aspect_ratio = height as f32 / width as f32;
-
-    let fov: f32 = std::f32::consts::PI / 2.0;
-    let zfar = 1024.0;
-    let znear = 0.1;
-
-    let f = 1.0 / (fov / 2.0).tan();
-
-    [
-        [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
-        [         0.0         ,     f ,              0.0              ,   0.0],
-        [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
-        [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
-    ]
-}
-
-pub fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    let s = [up[1] * f[2] - up[2] * f[1],
-             up[2] * f[0] - up[0] * f[2],
-             up[0] * f[1] - up[1] * f[0]];
-
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
-             f[2] * s_norm[0] - f[0] * s_norm[2],
-             f[0] * s_norm[1] - f[1] * s_norm[0]];
-
-    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
-}
 
 pub const VERTICES: [Vertex; 531] = [
     Vertex { position: (0.0, 0.0, 0.0) },   // dummy vector because in the original model indices
